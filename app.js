b5092822c15a4cce9c68291b6c29c4706bea8f80
GuangMei <liudehua@gmail.com>
2021-08-04 23:39:13 +0400

    xx

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var downloadRouter = require('./routes/download');
 var qrRouter = require('./routes/qr');
 
 var app = express();
@@ -18,7 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/download', downloadRouter);
 app.use('/qr', qrRouter);
 
 module.exports = app;
