c780a6bc934b21da873e6be619eead606caacdac
GuangMei <liudehua@gmail.com>
2021-08-04 21:11:51 +0400

    pc首页

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path')
+
+var is_mobile = function (req) {
+  var ua = req.get('User-Agent')
+  return /Android|webOS|iPhone|iPod|BlackBerry/i.test(ua);
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  let isMobile = is_mobile(req);
+  console.log(isMobile);
+  if (isMobile) {
+    var index = path.resolve(__dirname, '../public/mobile.html');
+    fs.readFile(index, 'utf8', (err, text) => {
+      res.send(text);
+    });
+  }
+  else {
+    var index = path.resolve(__dirname, '../public/pc.html');
+    fs.readFile(index, 'utf8', (err, text) => {
+      res.send(text);
+    });
+  }
 });
 
 module.exports = router;
