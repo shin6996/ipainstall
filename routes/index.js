b4f09d2b2ace009a85d7de6bb5405d95cec21ae6
shin6996 <dubayyshin@gmail.com>
2021-08-25 13:40:22 +0400

    xx

--- routes/index.js
+++ routes/index.js
@@ -20,15 +20,22 @@ var is_android = function (req) {
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let {invite_code} = req.query;
-  let copyObj = {"clipboard":"pilipili",invite_code}
+  let { invite_code, channel_id } = req.query;
+
+  var copyObj = { "clipboard": "pilipili" }
+
+  if (invite_code) {
+    copyObj = { "clipboard": "pilipili", invite_code }
+  }
+  else if (channel_id) {
+    copyObj = { "clipboard": "pilipili", channel_id }
+  }
 
   if (is_iOS(req)) {
-    res.render('ios', {invite_info:JSON.stringify(copyObj)});
+    res.render('ios', { copy_info: JSON.stringify(copyObj) });
   }
-  else if (is_android(req))
-  {
-    res.render('android', {invite_info:JSON.stringify(copyObj)});
+  else if (is_android(req)) {
+    res.render('android', { copy_info: JSON.stringify(copyObj) });
   }
   else {
     res.render('pc');
