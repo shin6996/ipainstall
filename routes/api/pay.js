31b24df75976dc3f840bb58b443d43a0b7a0c01a
shin6996 <dubayyshin@gmail.com>
2021-09-24 18:24:44 +0400

    xx

--- routes/api/pay.js
+++ routes/api/pay.js
@@ -9,7 +9,7 @@ router.get('/', async function (req, res, next) {
     const agent = new https.Agent({
         rejectUnauthorized: false
     });
-    const order_info = await axios.post('www.pipilolo.xyz/api/Notify/PaymentDiscountMember', {
+    const order_info = await axios.post('https://www.pipilolo.xyz/api/Notify/PaymentDiscountMember', {
         product_id: 10,
         channel_id: 8
     }, {
